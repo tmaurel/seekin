980d361a69e450d2ce446b2475d06362200f5578
Thomas Maurel <neoseifer@hotmail.com>
2010-01-27 19:39:29 +0100

    fixed cobertura exclusions

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,12 @@
+
+//cobertura exclusions
+coverage {
+	exclusions = [
+	'**/org/grails/**',
+	'**/*TagLib*/**',
+	'**/*gsp_seekin*',
+	'**/*GrailsUI*/**',
+	'**/*Navigation*/**',
+	'**/*Config*/**',
+	"**/*Tests*"]
+}
