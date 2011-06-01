b3f8e45eb5d027976513f50a3c44a37a32bbcb24
Nadir Boukeffa <nadir.boukeffa@gmail.com>
2011-06-02 01:30:10 +0200

    Commented YUI Buttons Tag Lib

---
+++ test/unit/me/hcl/seekin/Util/JQueryUISeekinTagLibTests.groovy
@@ -0,0 +1,17 @@
+package me.hcl.seekin.Util
+
+import grails.test.*
+
+class JQueryUISeekinTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
